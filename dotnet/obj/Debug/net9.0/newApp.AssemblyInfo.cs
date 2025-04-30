185cd1eda2a70ca02fa564ea753f79332ce94a2d
RebecaRakotomalala <159775871+RebecaRakotomalala@users.noreply.github.com>
2025-04-30 08:10:36 +0300

    commit

--- dotnet/obj/Debug/net9.0/newApp.AssemblyInfo.cs
+++ dotnet/obj/Debug/net9.0/newApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("newApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed74d0271ccc9c7a661782e7753b2de207c1364d")]
 [assembly: System.Reflection.AssemblyProductAttribute("newApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("newApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
